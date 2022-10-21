dc0f3d138d1951bb119fdd5cefcddf1a5195d17d
gmmunis <gmmunis@hotmail.com>
2022-10-21 12:01:12 -0300

    Busca em relacionamento muito para muitos

--- src/modules/createToZero.ts
+++ src/modules/createToZero.ts
@@ -1,27 +1,27 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-async function main() {
-    const result = await prisma.coursesModules.create({
-        data: {
-            course: {
-                create: {
-                    duration: 200,
-                    name: "Curso de NodeJS",
-                    description: "Curso completo de NodeJS",
-                },
-            },
-            module: {
-                create: {
-                    description: "Curso completo de PrismaIO",
-                    name: "PrismaIO",
-                },
-            },
-        },
-    });
+// async function main() {
+//     const result = await prisma.coursesModules.create({
+//         data: {
+//             course: {
+//                 create: {
+//                     duration: 200,
+//                     name: "Curso de NodeJS",
+//                     description: "Curso completo de NodeJS",
+//                 },
+//             },
+//             module: {
+//                 create: {
+//                     description: "Curso completo de PrismaIO",
+//                     name: "PrismaIO",
+//                 },
+//             },
+//         },
+//     });
 
-    console.log(result);
-}
+//     console.log(result);
+// }
 
-main();
+// main();
